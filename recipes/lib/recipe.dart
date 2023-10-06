ded0b41118792430d348265f907c2e9a7b9abe61
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2023-10-06 16:48:46 +0100

    Target page and the servings fucntion created thereby calculating the servings

--- recipes/lib/recipe.dart
+++ recipes/lib/recipe.dart
@@ -1,38 +1,147 @@
 class Recipe {
   String label;
   String imageUrl;
+  int servings;
+  List<Ingredient> ingredients;
+  // TODO: Add servings and ingredients here
 
-  Recipe(
-    this.label,
-    this.imageUrl,
-    // TODO: Add servings and ingredients here
-  );
-
+  Recipe(this.label, this.imageUrl, this.servings, this.ingredients);
+  // TODO; Add List<Recipe> here
   static List<Recipe> samples = [
     Recipe(
       'Spaghetti and Meatballs',
       'assets/images/food2.jpeg',
+      4,
+      [
+        Ingredient(
+          1,
+          'box',
+          "Spaghetti",
+        ),
+        Ingredient(
+          4,
+          '',
+          "Frozen Meatballs",
+        ),
+        Ingredient(
+          0.5,
+          'jar',
+          'sauce',
+        ),
+      ],
     ),
     Recipe(
       'Tomato Soup',
       'assets/images/food3.jpeg',
+      2,
+      [
+        Ingredient(
+          1,
+          'can',
+          'Tomato Soup',
+        ),
+      ],
     ),
     Recipe(
       'Grilled Cheese',
       'assets/images/Food4.jpeg',
+      1,
+      [
+        Ingredient(
+          2,
+          'slices',
+          'cheese',
+        ),
+        Ingredient(
+          2,
+          'slices',
+          "Bread",
+        ),
+      ],
     ),
     Recipe(
       'Chocolate Chip Cookies',
       'assets/images/food3.png',
+      24,
+      [
+        Ingredient(
+          4,
+          'cups',
+          'flour',
+        ),
+        Ingredient(
+          2,
+          'cups',
+          'sugar',
+        ),
+        Ingredient(
+          0.5,
+          'cups',
+          'chocolate chips',
+        ),
+      ],
     ),
     Recipe(
       'Taco Salad',
       'assets/images/Food4.png',
+      1,
+      [
+        Ingredient(
+          4,
+          'oz',
+          'nachos',
+        ),
+        Ingredient(
+          3,
+          'oz',
+          'tacho meat',
+        ),
+        Ingredient(
+          0.5,
+          'cup',
+          'cheese',
+        ),
+        Ingredient(
+          0.25,
+          'cup',
+          'chopped tomatoes',
+        ),
+      ],
     ),
     Recipe(
       'Hawaiian Pizza',
       'assets/images/Food5.png',
+      4,
+      [
+        Ingredient(
+          1,
+          'item',
+          'pizza',
+        ),
+        Ingredient(
+          1,
+          'cup',
+          'pineapple',
+        ),
+        Ingredient(
+          8,
+          'oz',
+          'ham',
+        )
+      ],
     ),
   ];
 }
+
 // TODO: Add Ingredient() here
+class Ingredient {
+  double quantity;
+  String measure;
+  String name;
+
+  Ingredient(
+    this.quantity,
+    this.measure,
+    this.name,
+  );
+}
--- recipes/lib/recipe_detail.dart
+++ recipes/lib/recipe_detail.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:recipes/recipe.dart';
-// import 'package:recipes/recipe.dart';
 
 class RecipeDetail extends StatefulWidget {
   const RecipeDetail({
@@ -16,6 +15,17 @@ class RecipeDetail extends StatefulWidget {
 // TODO: Add _RecipeDetailState here
 
 class _RecipeDetailState extends State<RecipeDetail> {
+  // TODO: Add _sliderVal here
+  int _sliderVal = 1;
+
+  void slider(newValue) {
+    setState(
+      () {
+        _sliderVal = newValue.round();
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,9 +48,32 @@ class _RecipeDetailState extends State<RecipeDetail> {
             Text(
               widget.recipe.label,
               style: const TextStyle(fontSize: 18),
-            )
+            ),
             // TODO: Add Expanded
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.all(7.0),
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: (context, index) {
+                  final ingredient = widget.recipe.ingredients[index];
+                  // TODO: Add ingredient.quantity
+                  return Text('${ingredient.quantity * _sliderVal}'
+                      '${ingredient.measure}'
+                      '${ingredient.name}');
+                },
+              ),
+            ),
             // TODO: Add Slider() here
+            Slider(
+              min: 1,
+              max: 10,
+              divisions: 10,
+              label: '${_sliderVal * widget.recipe.servings} servings',
+              value: _sliderVal.toDouble(),
+              onChanged: slider,
+              activeColor: Colors.green,
+              inactiveColor: Colors.black,
+            ),
           ],
         ),
       ),
